fbc0e51bfbbfa4c692c28a4e6d9379f594b1d823
Kadir Kuruca <kadirkuruca572@gmail.com>
2018-03-14 23:34:25 +0300

    Çıkış Yap Menüsü Eklendi ve Çıkış İşlemleri Yapıldı.

--- app/src/main/java/com/kadirkuruca/firebaseauthentication/MainActivity.kt
+++ app/src/main/java/com/kadirkuruca/firebaseauthentication/MainActivity.kt
@@ -1,12 +1,85 @@
 package com.kadirkuruca.firebaseauthentication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAuthStateListener : FirebaseAuth.AuthStateListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initAuthStateListener()
+    }
+
+    private fun initAuthStateListener() {
+        mAuthStateListener = object : FirebaseAuth.AuthStateListener{
+            override fun onAuthStateChanged(p0: FirebaseAuth) {
+                var kullanici = p0.currentUser
+                if(kullanici != null){
+
+                }else{
+                    var cikisIntent = Intent(this@MainActivity,LoginActivity::class.java)
+                    cikisIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(cikisIntent)
+                    finish()
+                }
+            }
+
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.anamenu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId){
+
+            R.id.cikisYap -> {
+                cikisYap()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun cikisYap() {
+        FirebaseAuth.getInstance().signOut()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        kullaniciyiKontrolEt()
+    }
+
+    private fun kullaniciyiKontrolEt() {
+        var kullanici = FirebaseAuth.getInstance().currentUser
+        if(kullanici == null){
+            var cikisIntent = Intent(this@MainActivity,LoginActivity::class.java)
+            cikisIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(cikisIntent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthStateListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(mAuthStateListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthStateListener)
+        }
     }
 }
